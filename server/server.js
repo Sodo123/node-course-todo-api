5c401880784594e2783343f05db0205001220581
duclt <duclt@ghtk.vn>
2019-01-17 23:40:31 +0700

    Add GEt /users/me and authentication middle ware

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ var { mongoose } = require('./db/mongoose');
 var { Todo }  = require('./models/todo');
 var { User }  = require('./models/user');
 const {ObjectID} = require('mongodb');
-
+var {authenticate} = require('./middleware/authenticate')
+;
 var app = express();
 
 app.use(bodyParser.json());
@@ -106,12 +107,18 @@ app.post('/users', (req, res) => {
     var body = _.pick(req.body,['email', 'password']);
     var user = new User(body);
     user.save().then(() => {
-      return user.generateAuthToken(); 
+      return user.generateAuthToken();
     }).then((token) => {
       res.header('x-auth', token).send(user);
     }).catch((e) => {
       res.status(400).send(e);
     });
+});
+
+
+
+app.get('/users/me',authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(3000, () => {
