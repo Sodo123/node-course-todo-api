63208ef53c000173c988dce734cce7bbe11f5e6e
duclt <duclt@ghtk.vn>
2019-01-20 23:43:45 +0700

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,26 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
+var hashedPassword = '$2a$10$YXg5J2Zy57mwpJgZIjmMr.YvZRK/SisVgnDPzJEFLHiPuXvMXEvj.';
 
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
