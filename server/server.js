eb1514975fe43a2239776a51c081d147bac1f773
duclt <duclt@ghtk.vn>
2019-01-21 23:54:42 +0700

    Add DELETE users/me/token route

--- server/server.js
+++ server/server.js
@@ -132,6 +132,14 @@ app.post('/users/login', (req, res) => {
     //res.send(req.body);
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 app.listen(3000, () => {
     console.log('Start on port 3000');
 });
