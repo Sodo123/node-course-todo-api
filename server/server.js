577f19b06dcd0360a58700c30e3246fbf08f7a56
duclt <duclt@ghtk.vn>
2019-01-21 23:10:16 +0700

    Add POST users/login route

--- server/server.js
+++ server/server.js
@@ -115,10 +115,21 @@ app.post('/users', (req, res) => {
     });
 });
 
-
-
 app.get('/users/me',authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body,['email', 'password']);
+    console.log(body);
+    User.findByCredentials(body.email, body.password).then((user) => {
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+      })
+    }).catch((e) => {
+      res.status(400).send();
+    });
+    //res.send(req.body);
 });
 
 app.listen(3000, () => {
